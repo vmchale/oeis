eaa74e864850027c6efa060f9be9b452679ad418
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-02 00:38:34 -0500

    api etc.

--- src/Num/OEIS.hs
+++ src/Num/OEIS.hs
@@ -1,5 +1,10 @@
 module Num.OEIS
-    ( write000127
+    ( -- * Helper functions
+      asBTextFile
+    -- * Specific sequences
+    , oeis000127
+    -- * Data generation
+    , write000127
     ) where
 
 import           Numeric.Combinatorics (choose)
@@ -7,8 +12,11 @@ import           Numeric.Combinatorics (choose)
 maxRegions :: Int -> Integer
 maxRegions n = sum $ fmap (n `choose`) [0..4]
 
+oeis000127 :: [Integer]
+oeis000127 = fmap maxRegions [0..45000]
+
 write000127 :: IO ()
-write000127 = writeFile "b000127.txt" (asBTextFile (maxRegions <$> [0..45000]))
+write000127 = writeFile "b000127.txt" (asBTextFile oeis000127)
 
 index :: [a] -> [(Int, a)]
 index = zip [1..]
