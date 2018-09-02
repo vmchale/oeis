147bcfea1e51b9188c245013959151ee750e7b0c
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-02 01:24:25 -0500

    add some sequences

--- src/Num/OEIS.hs
+++ src/Num/OEIS.hs
@@ -6,11 +6,21 @@ module Num.OEIS
     , oeis000004
     , oeis000007
     , oeis000012
+    , oeis000027
+    , oeis000030
+    , oeis000032
+    , oeis000034
+    , oeis000035
+    , oeis000038
+    , oeis000042
     , oeis000127
     ) where
 
 oeis000007 :: Integral a => [a]
 oeis000007 = 1 : repeat 0
+
+oeis000038 :: Integral a => [a]
+oeis000038 = 2 : repeat 0
 
 oeis000012 :: Integral a => [a]
 oeis000012 = repeat 1
@@ -30,6 +40,27 @@ oeis000002 = 1 : 2 : (drop 2 . concat . zipWith replicate oeis000002 . cycle) [1
 
 oeis000004 :: Integral a => [a]
 oeis000004 = repeat 0
+
+oeis000027 :: Integral a => [a]
+oeis000027 = [1..]
+
+oeis000032 :: Integral a => [a]
+oeis000032 = 2 : 1 : zipWith (+) oeis000032 (tail oeis000032)
+
+oeis000034 :: Integral a => [a]
+oeis000034 = cycle [1,2]
+
+oeis000035 :: Integral a => [a]
+oeis000035 = cycle [0,1]
+
+-- | Due to Reinhard Zumkeller
+oeis000030 :: Integral a => [a]
+oeis000030 = fmap initialDigit [1..]
+    where initialDigit = until (< 10) (`quot` 10)
+
+oeis000042 :: Integral a => [a]
+oeis000042 = fmap unary [(1::Word)..]
+    where unary = (`quot` 9) . subtract 1 . (10 ^)
 
 oeis000127 :: [Integer]
 oeis000127 = fmap maxRegions [0..]
