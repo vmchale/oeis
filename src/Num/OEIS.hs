dc9ba9f7c398de94af8fb5239f7725bf7d6a7876
Vanessa McHale <vanessa.mchale@iohk.io>
2018-09-02 01:11:05 -0500

    functions

--- src/Num/OEIS.hs
+++ src/Num/OEIS.hs
@@ -2,8 +2,18 @@ module Num.OEIS
     ( -- * Helper functions
       asBTextFile
     -- * Specific sequences
+    , oeis000002
+    , oeis000004
+    , oeis000007
+    , oeis000012
     , oeis000127
     ) where
+
+oeis000007 :: Integral a => [a]
+oeis000007 = 1 : repeat 0
+
+oeis000012 :: Integral a => [a]
+oeis000012 = repeat 1
 
 factorial :: Int -> Integer
 factorial n = product [1..(fromIntegral n)]
@@ -13,6 +23,13 @@ choose n k = product (fmap fromIntegral [(n-k+1)..n]) `div` factorial k
 
 maxRegions :: Int -> Integer
 maxRegions n = sum $ fmap (n `choose`) [0..4]
+
+-- | Due to John Tromp
+oeis000002 :: Integral a => [a]
+oeis000002 = 1 : 2 : drop 2 (concat . zipWith replicate oeis000002 . cycle $ [1, 2])
+
+oeis000004 :: Integral a => [a]
+oeis000004 = repeat 0
 
 oeis000127 :: [Integer]
 oeis000127 = fmap maxRegions [0..]
